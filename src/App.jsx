322a29464e1722cef0c31ff50241deadaa02d649
Shashwat2104 <shashwatmahender2104@gmail.com>
2023-11-23 13:24:59 +0530

    Assignment-2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import './App.css';
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant,
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant,
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant,
     },
     {
-      id:4,
-      img:elephant
-    }
-  ]
+      id: 4,
+      img: elephant,
+    },
+  ];
   return data;
-}
+};
 
 function App() {
-  // code here
+  const images = imageData();
+  return (
+    <div className="App">
+      <h1>Kalvium gallary</h1>
+      {images.map((el) => {
+        return (
+          <div key={el.id}>
+            <img src={el.img} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
